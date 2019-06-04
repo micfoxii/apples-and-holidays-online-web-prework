58f5a0c61bd25e6ce553a96effcc1cb312d93fe9
Michael Fox <michael.c.fox.ii@gmail.com>
2019-06-04 15:36:53 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -90,6 +90,7 @@ def all_holidays_with_bbq(holiday_hash)
         end
       end
     end
+  end
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 
